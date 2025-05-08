a623f15c49bbf05fcdf401715a1cc2f5d0989960
Aakash Kumar Suman <121715040+iamaakashks@users.noreply.github.com>
2025-05-08 23:54:02 +0530

    added the dister

---
+++ controller/user.js
@@ -0,0 +1,4 @@
+import { userModel } from "../model/user.model.js"
+export const loginUser = (req, res, next)=>{
+
+}
--- model/user.model.js
+++ model/user.model.js
@@ -1,1 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+})
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+export const userModel = mongoose.model('user', userSchema);
